0239013a1c7c38e50d5bc3646847b550c4032d7e
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-19 21:44:16 +0530

    animation aded to the page

--- front_end/src/compenets/Menus/Menus.jsx
+++ front_end/src/compenets/Menus/Menus.jsx
@@ -5,13 +5,15 @@ import Fruit4 from '../../assets/fruits/cherry.png'
 import Fruit2 from '../../assets/fruits/orange.png'
 //import FruitOne from '../../assets/fruits/apple.png'
 
+import {motion} from 'framer-motion';
+import {FadeLeft} from '../../utility/animation';
 
 
 const MenuData = [
-  { id: 1, title: 'Red Apple', link:Fruit1 ,price: '100.00' },
-  { id: 2, title: 'Fresh Oranges', link:Fruit2 , price: '50.00' },
-  { id: 3, title: 'Big Avocado', link: Fruit3, price: '75.00' },
-  { id: 4, title: 'Sweet Cherries', link:Fruit4, price: '100.00' },
+  { id: 1, title: 'Red Apple', link:Fruit1 ,price: '100.00' ,delay:0.3},
+  { id: 2, title: 'Fresh Oranges', link:Fruit2 , price: '50.00',delay:0.6 },
+  { id: 3, title: 'Big Avocado', link: Fruit3, price: '75.00',delay:0.9 },
+  { id: 4, title: 'Sweet Cherries', link:Fruit4, price: '100.00' ,delay:1.2},
 ];
 
 const Menus = () => {
@@ -19,19 +21,31 @@ const Menus = () => {
     <section className="bg-white py-12">
       <div className="container mx-auto px-4">
         {/* Section Header */}
-        <h1 className="text-3xl font-bold text-left pb-10 uppercase text-gray-800">
+        <motion.h1 
+        initial={{opacity:0 , x:200}}
+        whileInView={{opacity:1,x:0}}
+        transition={{duration:1,delay:0.5}}
+
+
+        className="text-3xl font-bold text-left pb-10 uppercase text-gray-800">
           Our Menu
-        </h1>
+        </motion.h1>
 
         {/* Responsive Grid Layout */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
           {MenuData.map((menu) => (
-            <div 
+            <motion.div 
+            variants={FadeLeft(menu.delay)}
+              initial='hidden'
+              whileInView={'visible'}
               key={menu.id} 
               className="bg-white rounded-3xl shadow-[0_0_22px_rgba(0,0,0,0.15)] p-6 flex flex-col items-center justify-center gap-3 hover:scale-110 transition-all duration-300 cursor-pointer"
             >
               {/* Placeholder for Fruit Image */}
-              <div className="w-20 h-20 bg-gray-100 rounded-full flex items-center justify-center text-3xl">
+              <div 
+              
+
+              className="w-20 h-20 bg-gray-100 rounded-full flex items-center justify-center text-3xl">
                 { <img src={menu.link}/>}
                 
               </div>
@@ -43,7 +57,7 @@ const Menus = () => {
                   Rs {menu.price}
                 </p>
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
