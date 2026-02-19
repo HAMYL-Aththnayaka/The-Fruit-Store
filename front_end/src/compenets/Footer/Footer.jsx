40a9c7b331909e6e997d53e0b1c9fb999c9070be
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-19 23:34:42 +0530

    Filling the footer skeleton

--- front_end/src/compenets/Footer/Footer.jsx
+++ front_end/src/compenets/Footer/Footer.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import {FaFacebookF,FaTwitter,FaInstagram ,FaLeaf} from 'react-icons/fa'
+import {mottion} from 'framer-motion';
+import fImage from '../../assets/footer-bg.jpg'
 
 const Footer =()=>{
-    return <></>
+    return <>
+        <footer className=''>
+        <div className='container flex justify-between items-center' >
+           
+           {/*Logo Section */}
+            <div className=' text-2xl flex items-center gap-2 font-bold uppercase'git>
+
+            </div>
+           {/*Social icons Section */}
+            <div className=''>
+
+            </div>
+
+        </div>
+        </footer>
+    </>
 }
 export default Footer
