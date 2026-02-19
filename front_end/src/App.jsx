68c47c5fb3cc3c88b93d256c249a91e2c2e80372
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-19 23:04:42 +0530

    The Banner3 skeloton has been created and added to the main app.jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './compenets/Hero/Hero'
 import Menus from './compenets/Menus/Menus'
 import Banner  from './compenets/Banner/Banner.jsx'
 import Banner2  from './compenets/Banner/Banner2.jsx'
+import Banner3  from './compenets/Banner/Banner3.jsx'
 
 const App= ()=><>
   <main className='overflow-x-hidden'>
@@ -12,6 +13,7 @@ const App= ()=><>
     <Menus/>
     <Banner/>
     <Banner2/>
+    <Banner3/>
   </main>
 </>
 
---
+++ front_end/src/compenets/Banner/Banner3.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Banner3 =()=>{
+    return<></>
+}
+
+export default Banner3;
